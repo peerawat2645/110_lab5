0479991af8316c52ddd5a724e75a7b72d59aa5ea
Peerawat Wongmek <peerawat.won@ku.th>
2021-03-08 17:42:15 +0700

    add

---
+++ 3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int isUgly(int n){
+    if (n==1) return 1;
+    if (n<=0) return 0;
+    if (n%2==0) return (isUgly(n/2));
+    if (n%3==0) return (isUgly(n/3));
+    if (n%5==0) return (isUgly(n/5));
+    return 0;
+}
+int main()
+{
+    int n,i,j,k=0;
+    scanf("%d",&n);
+    int a[n*n];
+    for(i=1;i<n*n;i++,k++)
+    {
+        j=isUgly(i);
+        if(j==1)
+        {
+            a[k]=i;
+        }
+        else
+            k--;
+    }
+    printf("%d",a[n-1]);
+
+    return 0;
+}
