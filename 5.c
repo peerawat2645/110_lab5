b9deba5201af9ad16c267ab57a0ac49e0e6a25de
Peerawat Wongmek <peerawat.won@ku.th>
2021-03-08 20:03:26 +0700

    add

--- 5.c
+++ 5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n,m,t,i,j,k,l,max;
+    int n,m,t,i,j,k,l,max=-50000,p;
     scanf("%d %d",&n,&m);
     int a[n][n],b[m][m];
     t=m-1;
@@ -10,12 +10,18 @@ int main()
             scanf("%d",&a[i][j]);
     for(i=0;i<n-t;i++)
         for(j=0;j<n-t;j++){
+            p=0;
             for(k=0;k<m;k++)
                 for(l=0;l<m;l++){
-                    b[k][l];
+                    b[k][l]=a[i+k][j+l];
+                    p+=b[k][l];
                 }
+            if(p>max)
+            {
+                max=p;
+            }
         }
 
-
+    printf("%d",max);
     return 0;
 }
